f1db203e5a547cdd4eeefbefc02173c7f5c49da2
Jadranko Dragoje <dragoje.jadranko@nsoft.com>
2019-12-31 15:29:46 +0100

    Optimise connection

--- src/lambda/board.js
+++ src/lambda/board.js
@@ -2,14 +2,29 @@ import { MongoClient } from 'mongodb';
 
 const dbUri = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_CLUSTER}?retryWrites=true&w=majority`;
 
-export async function handler(event, context) {
-  const dbClient = new MongoClient(dbUri, { useNewUrlParser: true });
+let dbCachedConnection = null;
+
+const connectToDb = async () => {
+  if (dbCachedConnection) {
+    return dbCachedConnection;
+  }
+
+  const dbClient = new MongoClient(dbUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
   await dbClient.connect();
-  const dbBoardCollection = dbClient.db('workboard').collection('board');
+
+  dbCachedConnection = dbClient.db('workboard');
+
+  return dbCachedConnection;
+};
+
+export async function handler(event, context) {
+  const dbConnection = await connectToDb();
+  const dbBoardCollection = dbConnection.collection('board');
   const dbBoardDocs = await dbBoardCollection.find({}).toArray();
-  
-  dbClient.close();
 
   return {
     statusCode: 200,
