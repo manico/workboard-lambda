d6217691b6725448413af2727445d5d6e03664e3
Jadranko Dragoje <dragoje.jadranko@nsoft.com>
2019-12-31 14:49:51 +0100

    Remove event

--- src/lambda/echo.js
+++ src/lambda/echo.js
@@ -2,7 +2,6 @@ export async function handler(event, context) {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      event: event,
       workingDirectory: process.env.PWD,
       netlifyDev: process.env.NETLIFY_DEV,
       nodeEnv: process.env.NODE_ENV,
