82c4702dcd7c49b8a135be11788af38c2c4ddcb8
Jadranko Dragoje <dragoje.jadranko@nsoft.com>
2019-12-31 13:08:25 +0100

    Setup lambda

---
+++ src/lambda/echo.js
@@ -0,0 +1,10 @@
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      workingDirectory: process.env.PWD,
+      netlifyDev: process.env.NETLIFY_DEV,
+      nodeEnv: process.env.NODE_ENV,
+    }),
+  };
+};
