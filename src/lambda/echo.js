0ab962940dadf33febd89f3589b9005c3f4d2833
Jadranko Dragoje <dragoje.jadranko@nsoft.com>
2019-12-31 14:39:53 +0100

    Add event

--- src/lambda/echo.js
+++ src/lambda/echo.js
@@ -2,6 +2,7 @@ export async function handler(event, context) {
   return {
     statusCode: 200,
     body: JSON.stringify({
+      event: event,
       workingDirectory: process.env.PWD,
       netlifyDev: process.env.NETLIFY_DEV,
       nodeEnv: process.env.NODE_ENV,
