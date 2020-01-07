be5955d95df461a7571dfe339602ba6f468c4f10
Jadranko Dragoje <dragoje.jadranko@nsoft.com>
2020-01-07 16:48:25 +0100

    Modify board function

--- src/lambda/board.js
+++ src/lambda/board.js
@@ -3,12 +3,19 @@ import db from './utility/db';
 export async function handler(event, context) {
   context.callbackWaitsForEmptyEventLoop = false;
 
+  const query = event.queryStringParameters;
+  const queryId = query.id || query._id;
+
   const dbConnection = await db.connect();
   const dbBoardCollection = dbConnection.collection('board');
-  const dbBoardDocs = await dbBoardCollection.find({}).toArray();
+
+  const dbBoardDoc = await dbBoardCollection
+    .findOne({
+      _id: db.getIdBinary(queryId),
+    });
 
   return {
     statusCode: 200,
-    body: JSON.stringify(dbBoardDocs),
+    body: JSON.stringify(dbBoardDoc),
   };
 }
--- src/lambda/utility/db.js
+++ src/lambda/utility/db.js
@@ -1,9 +1,12 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectID } from 'mongodb';
 
 const dbUri = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_CLUSTER}?retryWrites=true&w=majority`;
 
 export default {
   connection: null,
+  getIdBinary(idHex) {
+    return ObjectID.createFromHexString(idHex);
+  },
   async connect() {
     if (this.connection) {
       return this.connection;
